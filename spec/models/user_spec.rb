e91ebd208d78ef1ae080a84053cdc502cf425515
cordelia <cordelia@example.com>
2021-08-19 16:47:03 +0900

    add Rspec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it '1 + 1 = 2' do
+    expect(1 + 1 ).to eq 2
+  end
+end
